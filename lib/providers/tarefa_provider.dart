dc54f45d2f5ed61cdb542003b0a4b25d0ed343c9
Amp&rsand <muriloschneider23@gmail.com>
2026-04-30 21:30:05 -0300

    Criação do Banco de Dados sqlite

--- lib/providers/tarefa_provider.dart
+++ lib/providers/tarefa_provider.dart
@@ -8,3 +8,30 @@ Requisitos e item que serão avaliados:
 6. Deve ser feito gerenciamento de estado com o Provider (não precisa ser todas informações
 gerenciadas com o Provider, mas a que se demonstrarem mais relevantes).
 */
+import 'package:flutter/material.dart';
+import 'package:ifsc_mobile_av1_crud/util/db.dart';
+import 'package:ifsc_mobile_av1_crud/models/tarefa.dart';
+
+class TarefaProvider extends ChangeNotifier {
+  List<Tarefa> _tarefas = [];//lista de tarefas gerenciada pelo provider
+  List<Tarefa> get tarefas => _tarefas;//getter que acessa a lista de tarefas
+
+  //CRUD (utilizando os métodos do DBUtil -> util.db.dart)
+  Future<void> addTarefa(Tarefa tarefa) async {
+    await DBUtil.create(tarefa.toMap());
+    await loadTarefas();
+  }
+  Future<void> loadTarefas() async {
+    final tarefasRetornadas = await DBUtil.read('');
+    _tarefas = tarefasRetornadas.map((item) => Tarefa.fromMap(item)).toList();
+    notifyListeners();
+  }
+  Future<void> updateTarefa(int id, Tarefa tarefa) async {
+    await DBUtil.update(id, tarefa.toMap());
+    await loadTarefas();
+  }
+  Future<void> deleteTarefa(int id) async {
+    await DBUtil.delete(id);
+    await loadTarefas();
+  }
+}
--- lib/util/db.dart
+++ lib/util/db.dart
@@ -4,3 +4,50 @@ banco, faça a classe de modelo ter um factory fromMap e um método toMap para f
 acesso ao banco de dados. Utilize id auto gerado pelo banco. Lembre-se que a data deve ser
 armazenada como Text, pesquise como você pode fazer essa transformação.
 */
+import 'package:sqflite/sqflite.dart' as sqlite;
+import 'package:path/path.dart' as path;
+import 'package:provider/provider.dart';
+
+class DBUtil{
+  static Future<sqlite.Database> getDatabase() async {
+    final databasePath = await sqlite.getDatabasesPath();//salva o caminho para o diretório do bd
+    final dbPath = path.join(databasePath, 'tarefas.db');//cria o caminho completo para o bd
+
+    //abre o banco de dados, se não existir ele é criado
+    return await sqlite.openDatabase(
+      dbPath,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE tarefas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT,
+            descricao TEXT,
+            responsavel TEXT,
+            dataPrevista TEXT,
+            importante INTEGER,
+            realizada INTEGER
+          )
+        ''');
+      },
+    );
+  }
+
+  //CRUD
+  static Future<void> create(Map<String, dynamic> tarefa) async {
+    final db = await getDatabase();
+    await db.insert('tarefas', tarefa);
+  }
+  static Future<List<Map<String, dynamic>>> read(String busca) async {
+    final db = await getDatabase();
+    return await db.query('tarefas', where: 'titulo LIKE ?', whereArgs: ['%$busca%']);
+  }
+  static Future<void> update(int id, Map<String, dynamic> tarefa) async {
+    final db = await getDatabase();
+    await db.update('tarefas', tarefa, where: 'id = ?', whereArgs: [id]);
+  }
+  static Future<void> delete(int id) async {
+    final db = await getDatabase();
+    await db.delete('tarefas', where: 'id = ?', whereArgs: [id]);
+  }
+}
