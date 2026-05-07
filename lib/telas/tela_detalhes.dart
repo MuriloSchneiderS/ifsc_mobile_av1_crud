5e0735893ecc923b4f18b4d8483ee91cce49fce4
Amp&rsand <muriloschneider23@gmail.com>
2026-05-07 19:49:07 -0300

    Update tela_detalhes.dart

--- lib/telas/tela_detalhes.dart
+++ lib/telas/tela_detalhes.dart
@@ -3,6 +3,7 @@
 realizar a tarefa.
 */
 import 'package:flutter/material.dart';
+import 'package:ifsc_mobile_av1_crud/models/tarefa.dart';
 
 class TelaDetalhes extends StatelessWidget {
   final String titulo;
@@ -10,12 +11,34 @@ class TelaDetalhes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //! programador garante que nao sera nulo, pois tem uma rota associada
+    Tarefa tarefa = ModalRoute.of(context)?.settings.arguments as Tarefa ;
     return Scaffold(
       appBar: AppBar(
         title: Text(titulo),
       ),
-      body: const Center(
-        child: Text('Detalhes da Tarefa'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            Text("tela DETALHES", 
+              style: TextStyle(fontSize: 24)
+              ),
+            Text("Título: ${tarefa.titulo}"),
+            Text("Descrição: ${tarefa.descricao}"),
+            Text("Responsável: ${tarefa.responsavel}"),
+            Text("Data Prevista: ${tarefa.dataPrevista.toLocal()}"),
+            Text("Importante: ${tarefa.importante ? 'Sim' : 'Não'}"),
+            Text("Realizada: ${tarefa.realizada ? 'Sim' : 'Não'}"),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        tooltip: 'voltar',
+        child: const Icon(Icons.arrow_back_sharp),
       ),
     );
   }
